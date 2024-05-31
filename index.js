6a45faeb7148b266ad842850db64d4fc1a543570
Arya Samik <aryasamik@gmail.com>
2024-05-31 15:02:54 +0530

    modify index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post("/api/user", asyncWrap(async(req, res, next) => {
 
 app.post("/api/user/login", asyncWrap( async (req, res) => {
     let {email, password} = req.body;
-    if(!req.body || email=="" || password==""){
+    if(isEmpty(req.body) || email=="" || password==""){
         throw new ExpressError(400, "Invalid Credentials")
     }
     let user = await User.find({email: email});
